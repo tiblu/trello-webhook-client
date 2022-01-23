2fdb6a76b5376d25b1531c750d9a88c3105a93b5
Mikk Andresen <mikk.andresen@gmail.com>
2022-01-23 19:14:55 +0200

    removeCheclistFromCard

--- app.js
+++ app.js
@@ -228,7 +228,7 @@ app.post('/api/trello/webhooks/masterlist', async function (req, res) {
 
             const checkItemsToDelete = checkItemsOnMaster.filter((checkItem) => {
                 let checkItemNameRegex = new RegExp(`.*|${checklist.id}|${checkItem.id}\\)`);
-                `checkItem`.name.match(checkItemNameRegex);
+                checkItem.name.match(checkItemNameRegex);
             });
 
             logger.info('MASTER ITEMS TO DELETE', JSON.stringify(checkItemsToDelete, null, 2));
